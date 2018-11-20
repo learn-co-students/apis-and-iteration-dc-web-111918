c401e99579cb04b38bf0d0eaa4444ee5bf1e2b7a
rnovick13 <rnovick13@gmail.com>
2018-11-20 17:40:48 -0500

    the force was strong with us

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,21 +6,38 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  films = nil
+  response_hash["results"].each do |element|
+    if element["name"].downcase == character_name
+          films = element["films"]
+    end #if
+  end #response each
+  return films
+end #method
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+# get_character_movies_from_api("luke skywalker")
+
+# iterate over the response hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `print_movies`
+#  and that method will do some nice presentation stuff like puts out a list
+#  of movies by title. Have a play around with the puts with other info about a given film.
+# films = get_character_movies_from_api(character_name)
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
+  films.each do |url|
+    film_list_string = RestClient.get(url)
+    film_hash = JSON.parse(film_list_string)
+    # binding.pry
+    puts film_hash["title"]
+  end
+end    # some iteration magic and puts out the movies in a nice list
+
+# print_movies(films)
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
